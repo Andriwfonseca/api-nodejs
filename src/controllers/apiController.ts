8e7b55db34b55a7cd4d361eda6ffbe7bb013e245
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-03-15 20:08:11 -0300

    add createPhrase

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -2,16 +2,24 @@ import { Request, Response } from 'express';
 
 import { Phrase } from '../models/Phrase';
 
-export const ping = (req: Request, res: Response) =>{
-    res.json({pong: true});
+export const ping = (req: Request, res: Response) => {
+    res.json({ pong: true });
 }
 
-export const random = (req: Request, res: Response) =>{
-    let nRand: number = Math.floor( Math.random() * 10);
-    res.json({number: nRand});
+export const random = (req: Request, res: Response) => {
+    let nRand: number = Math.floor(Math.random() * 10);
+    res.json({ number: nRand });
 }
 
-export const nome = (req: Request, res: Response) =>{
+export const nome = (req: Request, res: Response) => {
     let nome: string = req.params.nome;
-    res.json({nome: `Você enviou o nome ${nome}`});
+    res.json({ nome: `Você enviou o nome ${nome}` });
+}
+
+export const createPhrase = async (req: Request, res: Response) => {
+    let { author, txt } = req.body;
+
+    let newPhrase = await Phrase.create({ author, txt });
+
+    res.json({ id: newPhrase.id, author, txt });
 }
