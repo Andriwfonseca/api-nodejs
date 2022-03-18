2e74986f0e783214ba11cd2090d4257fc376c95a
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-03-18 18:32:57 -0300

    added new routes

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+import { Sequelize } from 'sequelize';
 import { Phrase } from '../models/Phrase';
 
 export const ping = (req: Request, res: Response) => {
@@ -25,20 +25,59 @@ export const createPhrase = async (req: Request, res: Response) => {
     res.json({ id: newPhrase.id, author, txt });
 }
 
-export const listPhrases =async (req: Request, res: Response) => {
+export const listPhrases = async (req: Request, res: Response) => {
     let list = await Phrase.findAll();
 
     res.json(list);
 }
 
-export const getPhrase =async (req: Request, res: Response) => {
+export const getPhrase = async (req: Request, res: Response) => {
     let { id } = req.params;
 
     let phrase = await Phrase.findByPk(id);
 
     if(phrase){
-        res.json(phrase);
+        res.json( {phrase} );
     }else{
         res.json({ error: 'Frase não encontrada.' });
+    }
+}
+
+export const updatePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let { author, txt } = req.body;
+
+    let phrase = await Phrase.findByPk(id);
+
+    if(phrase){
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+
+        res.json( {phrase} );
+    }else{
+        res.json({ error: 'Frase não encontrada.' });
+    }
+}
+
+export const deletePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    
+    await Phrase.destroy( { where: { id } } );    
+
+    res.json({});
+}
+
+export const randomPhrase = async (req: Request, res: Response) => {
+    let phrase = await Phrase.findOne({
+        order: [
+            Sequelize.fn('RAND')
+        ]
+    });
+
+    if(phrase){
+        res.json( {phrase} );
+    }else{
+        res.json({ error: 'Não há frases cadastrada.' });
     }
 }
